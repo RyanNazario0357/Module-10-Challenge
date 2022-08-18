2e11df882fce24ae7e89c2ba7d0f5151ddf1e233
RyanNazario0357 <Ryan0357@icloud.com>
2022-08-18 19:52:52 -0400

    created const and test for engineer file

---
+++ _test_/Engineer.test.js
@@ -0,0 +1,6 @@
+const Engineer = require('../lib/Engineer');
+
+test('creates engineer object from employee', () => {
+    const engineer = new Engineer('Steve, 1, "steve@domain.com', 'tvoisyabr');
+
+})
