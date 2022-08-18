1b033698d79895f15cca24d61b74d4a6f3c419c6
RyanNazario0357 <Ryan0357@icloud.com>
2022-08-18 19:30:51 -0400

    added console.logs for employee name,ID, and email

--- _test_/Employee.test.js
+++ _test_/Employee.test.js
@@ -6,4 +6,8 @@ test('creates an employee object', () => {
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
-})
+
+    console.log('Employee Name: ${employee.name}');
+    console.log('Employee ID: ${employee.id}');
+    console.log('Employee E-Mail: ${employee.email}');
+});
