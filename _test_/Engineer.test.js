511c3bd87ef67efafb7231a8fef54df21c1053fd
RyanNazario0357 <Ryan0357@icloud.com>
2022-08-18 19:54:50 -0400

    created expect command for email,name and id

--- _test_/Engineer.test.js
+++ _test_/Engineer.test.js
@@ -3,4 +3,7 @@ const Engineer = require('../lib/Engineer');
 test('creates engineer object from employee', () => {
     const engineer = new Engineer('Steve, 1, "steve@domain.com', 'tvoisyabr');
 
+    expect(engineer.name).toEqual(expect.any(String));
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toEqual(expect.any(String));
 })
