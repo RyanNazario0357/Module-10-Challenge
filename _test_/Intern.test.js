9fde63a5af70fd542536b33daf1b34a73b8ed185
RyanNazario0357 <Ryan0357@icloud.com>
2022-08-18 20:08:52 -0400

    copy and pasted from engineer file to edit in intern file

---
+++ _test_/Intern.test.js
@@ -0,0 +1,13 @@
+const Intern = require('../lib/Engineer');
+
+test('creates engineer object from employee', () => {
+    const engineer = new Engineer('Steve, 1, "steve@domain.com', 'tvoisyabr');
+
+    expect(engineer.name).toEqual(expect.any(String));
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toEqual(expect.any(String));
+
+    console.log(`Engineer Name: ${engineer.name}`);
+    console.log(`Engineer ID: ${engineer.id}`);
+    console.log(`Engineer Email: ${engineer.email}`);
+});
