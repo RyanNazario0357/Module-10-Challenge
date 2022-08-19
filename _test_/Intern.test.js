07dee6c3c96702540939e644889008c97c372de2
RyanNazario0357 <Ryan0357@icloud.com>
2022-08-18 20:16:15 -0400

    copy and pasted from engineer file to edit in intern file

--- _test_/Intern.test.js
+++ _test_/Intern.test.js
@@ -1,4 +1,4 @@
-const Intern = require('../lib/Engineer');
+const Intern = require('../lib/Intern');
 
 test('create intern object from employee', () => {
     const intern = new Intern('Steve, 1, "steve@domain.com', 'UCF');
@@ -10,4 +10,14 @@ test('create intern object from employee', () => {
     console.log(`Intern Name: ${intern.name}`);
     console.log(`Intern ID: ${intern.id}`);
     console.log(`Intern Email: ${intern.email}`);
+});
+
+test('recieve github information', () => {
+    const engineer = new Engineer('Steve', 1, 'steve@domain.com', 'tvoisyabr');
+    expect(engineer.github).toEqual(expect.any(String));
+});
+
+test('create engineer role', () => {
+    const engineer = new Engineer('Steve', 1, 'steve@domain.com', 'tvoisyabr');
+    expect(engineer.getRole()).toBe('Engineer');
 });
