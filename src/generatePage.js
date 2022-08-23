2a4fb2e8ea8390eacd16d0bc081fad5576a61ca5
RyanNazario0357 <Ryan0357@icloud.com>
2022-08-23 18:13:48 -0400

    added push employee info, and started manager function

---
+++ src/generatePage.js
@@ -0,0 +1,13 @@
+const generateCards = data => {
+    cardArray = [];
+
+    for (let i = 0; i < data.length; i++) {
+        let employee = data[i];
+        let role = employee.getrole();
+
+        if (role === 'Manager') {
+            const managerInfo = generateManager(employee);
+            cardArray.push(managerinfo);
+        }
+    }
+} 
