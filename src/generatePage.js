d63484ef92a444f6f4d8cf33d32b6cb0b68948d7
RyanNazario0357 <Ryan0357@icloud.com>
2022-08-23 18:49:37 -0400

    added html code

--- src/generatePage.js
+++ src/generatePage.js
@@ -69,3 +69,17 @@ const generateIntern = intern => {
     </div>
     `;
 };
+
+const generatePage = data => {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <title>Team Profile</title>
+</head>
+<body>
+    `
+}
