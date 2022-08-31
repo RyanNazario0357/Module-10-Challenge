106a117dd77745c934fc03032121832e95dbffe5
RyanNazario0357 <Ryan0357@icloud.com>
2022-08-30 20:19:07 -0400

    started index file

---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require('inquirere');
+const fs = require('fs');
+const generateCards = require('./src/generatePage.js');
+
+const Manager = require('./lib/Manager.js');
+const Engineer = require('./lib/Engineer.js');
+const Intern = require('./lib/Intern.js');
+
