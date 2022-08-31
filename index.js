6ab8c9e6a3521843b2583b6d966a12b8db3ac9c5
RyanNazario0357 <Ryan0357@icloud.com>
2022-08-30 20:25:22 -0400

    started code to add manager info

--- index.js
+++ index.js
@@ -6,3 +6,28 @@ const Manager = require('./lib/Manager.js');
 const Engineer = require('./lib/Engineer.js');
 const Intern = require('./lib/Intern.js');
 
+const teamArray = [];
+
+const addManager = () => {
+    console.log(`
+    ==================
+    Add a Team Manager
+    ==================
+    `);
+    return inquirer.prompt([
+        {
+            type:'input',
+            name: 'name',
+            message: 'Team Manager Name',
+            validate: managerName => {
+                if (managerName) {
+                    return true;
+                } else {
+                    console.log('Enter Name');
+                }
+                }
+            }
+        };
+        {}
+    ])
+}
